c614e4ee92c7ede1efc3c0df259568d31b15cda3
alexandercleasby <cleasby.alex@gmail.com>
2018-12-26 12:37:32 -0500

    changed name of and build out module for making api requests

---
+++ src/api-requests/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export function activityget(year,month,day,lookback){
+    return new Promise((resolve,reject)=>{axios.get('trackerapi/getactivities?year='+year+'&month='+month+'&day='+day+'&lookback='+lookback).then(
+        (res)=>{
+            console.log(res.data)
+            resolve(res.data)
+        }
+    )
+}
+)}
+
